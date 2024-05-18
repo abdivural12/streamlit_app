7321672605d344ec5f1637f6f4113c9eb54657bc
abdivural12 <32742512+abdivural12@users.noreply.github.com>
2024-05-18 20:01:10 +0200

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,6 @@
 from langchain.agents.agent_types import AgentType
 from langchain_experimental.agents.agent_toolkits import create_csv_agent
-from langchain_openai import ChatOpenAI, OpenAI
+from langchain_openai import OpenAI  # Suppression de ChatOpenAI car il semble redondant
 from dotenv import load_dotenv
 import os
 import streamlit as st
@@ -8,7 +8,7 @@ import streamlit as st
 def main():
     load_dotenv()
 
-    # Récupérer la valeur de la variable d'environnement
+    # Récupérer la valeur de la clé API depuis les secrets Streamlit
     api_key = st.secrets["OPEN_API_KEY"]
 
     st.set_page_config(page_title="Ask your CSV")
@@ -16,15 +16,17 @@ def main():
 
     csv_file = st.file_uploader("Upload a CSV file", type="csv")
     if csv_file is not None:
+        try:
+            agent = create_csv_agent(
+                OpenAI(api_key=api_key, temperature=0), csv_file, verbose=True)
 
-        agent = create_csv_agent(
-            OpenAI(api_key=api_key, temperature=0), csv_file, verbose=True)
+            user_question = st.text_input("Ask a question about your CSV: ")
 
-        user_question = st.text_input("Ask a question about your CSV: ")
-
-        if user_question is not None and user_question != "":
-            with st.spinner(text="In progress..."):
-                st.write(agent.run(user_question))
+            if user_question:
+                with st.spinner(text="In progress..."):
+                    st.write(agent.run(user_question))
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     main()
