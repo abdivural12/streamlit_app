499219ad97acef77860963dd146cb7e70d86acbe
abdivural12 <32742512+abdivural12@users.noreply.github.com>
2024-05-18 19:58:52 +0200

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,20 +1,15 @@
 from langchain.agents.agent_types import AgentType
 from langchain_experimental.agents.agent_toolkits import create_csv_agent
 from langchain_openai import ChatOpenAI, OpenAI
-from langchain.llms import OpenAI
 from dotenv import load_dotenv
 import os
 import streamlit as st
 
-headers = {
-    "authorization": st.secrets["OPEN_API_KEY"],
-    "content-type": "application/json"
-}
 def main():
     load_dotenv()
 
-# Récupérer la valeur de la variable d'environnement
-    api_key = os.getenv('OPENAI_API_KEY')
+    # Récupérer la valeur de la variable d'environnement
+    api_key = st.secrets["OPEN_API_KEY"]
 
     st.set_page_config(page_title="Ask your CSV")
     st.header("Ask your CSV 📈")
@@ -23,14 +18,13 @@ def main():
     if csv_file is not None:
 
         agent = create_csv_agent(
-            OpenAI(temperature=0), csv_file, verbose=True)
+            OpenAI(api_key=api_key, temperature=0), csv_file, verbose=True)
 
         user_question = st.text_input("Ask a question about your CSV: ")
 
         if user_question is not None and user_question != "":
             with st.spinner(text="In progress..."):
                 st.write(agent.run(user_question))
-
 
 if __name__ == "__main__":
     main()
