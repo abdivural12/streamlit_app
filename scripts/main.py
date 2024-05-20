9ddf5cee6ade7dece717ae70d6e5978ff110d482
abdivural12 <32742512+abdivural12@users.noreply.github.com>
2024-05-20 20:10:20 +0200

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -5,21 +5,23 @@ from langchain_openai import OpenAI
 from dotenv import load_dotenv
 import os
 
-# Ajout des images et du texte explicatif en haut de la page
-st.markdown('''
-![alt text](https://moodle.msengineering.ch/pluginfile.php/1/core_admin/logo/0x150/1643104191/logo-mse.png "MSE Logo") 
-![alt text](https://www.hes-so.ch/typo3conf/ext/wng_site/Resources/Public/HES-SO/img/logo_hesso_master_tablet.svg "Hes Logo")
-
-Cette application a été développée dans le cadre d'un projet d'approfondissement visant à traiter les séries temporelles avec des modèles de langage (LLM). Elle utilise un agent CSV de la bibliothèque LangChain. Cette application permet aux utilisateurs d'interagir avec des fichiers CSV. Par exemple, elle peut répondre à des questions telles que "Combien y a-t-il de stations ?" ou "Quelle est la ville la plus chaude ?".
-''')
-
 def main():
     load_dotenv()
+
+    # Configuration de la page
+    st.set_page_config(page_title="Ask your CSV")
+
+    # Ajout des images et du texte explicatif en haut de la page
+    st.markdown('''
+    ![MSE Logo](https://moodle.msengineering.ch/pluginfile.php/1/core_admin/logo/0x150/1643104191/logo-mse.png)
+    ![HES Logo](https://www.hes-so.ch/typo3conf/ext/wng_site/Resources/Public/HES-SO/img/logo_hesso_master_tablet.svg)
+
+    Cette application a été développée dans le cadre d'un projet d'approfondissement visant à traiter les séries temporelles avec des modèles de langage (LLM). Elle utilise un agent CSV de la bibliothèque LangChain. Cette application permet aux utilisateurs d'interagir avec des fichiers CSV. Par exemple, elle peut répondre à des questions telles que "Combien y a-t-il de stations ?" ou "Quelle est la ville la plus chaude ?".
+    ''')
 
     # Récupérer la valeur de la clé API depuis les secrets Streamlit
     api_key = st.secrets["OPEN_API_KEY"]
 
-    st.set_page_config(page_title="Ask your CSV")
     st.header("Ask your CSV 📈")
 
     csv_file = st.file_uploader("Upload a CSV file", type="csv")
